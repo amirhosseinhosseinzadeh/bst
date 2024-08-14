e35211a1f0e824bfaf473126e11f02b6272411c1
hosseinzadeh <hosseinzadeh@HOSSEINZADEH-PC>
2024-08-14 09:49:21 +0330

    dev: create project base structure

---
+++ src/Libraries/BST.Application/Class1.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace BST.Application
+{
+    public class Class1
+    {
+
+    }
+}
---
+++ src/Libraries/BST.Core/Class1.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace BST.Core
+{
+    public class Class1
+    {
+
+    }
+}
